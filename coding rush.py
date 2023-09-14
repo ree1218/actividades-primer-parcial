a4089859d51882e240a642889ab5e2596734693d
Regina Navarro Álvarez <145035896+ree1218@users.noreply.github.com>
2023-09-14 13:41:38 -0600

    Create coding rush.py

---
+++ coding rush.py
@@ -0,0 +1,9 @@
+def funcion_invertidora():
+  print("¡Hey! Este es un programa que invierte las palabras que introduzcas")
+  print("Podemos empezar por invertir tu nombre. ¿Como te llamas?")
+  juanito = input()
+  print("Bien juanito ¿que te parece si ahora lo invertimos? Escribe la palabra juanito")
+  juanito = input()
+  palabra_invertida = juanito[::-1]
+  print(palabra_invertida)
+funcion_invertidora()
